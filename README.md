8e9a3eff68adc357977d0b82f85a30afe3dea09d
ImamDroubi <imam.droubi@gmail.com>
2023-10-23 19:47:13 +0300

    Initial Config

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ninety Plus
+
+Ninety Plus is an LMS for Palestinian 12th grade students, it connects students with teachers through live lessons.
+
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="">
+        APP
+      </div>
+    </>
+  )
+}
+
+export default App
